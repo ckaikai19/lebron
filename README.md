ac4ca455ed23230b79173b3b44636d531b55bf97
chirstiankaikai <ckaikai19@gmail.com>
2022-05-25 21:20:47 -0400

    done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# lebron
+# Module 4 Challenge - Christian Kaikai
+
+
+## Acceptance Criteria Checklist
+
+- When start button is clicked, the game begins and the timer starts
+- When a question is asked it tells the user if they got it correct or not 
+- When the answer is incorrect, the background color changes to red
+- When the answer is correct, the background color changes to green
+- When all questions are answered or the timer reaches 0
+- When the game has ended, the user can view previous stats
+
+
+[View depolyed project here](https://ckaikai19.github.io/lebron/)
+
+---
+
+## Mockup
+![website mockup](./img/mockup.png)
+
+---
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// grabbing all buttons from the dom
 const start_button = document.getElementById("startbut");
 const question_DOM = document.getElementById("question");
 const next_button = document.getElementById("next");
@@ -11,6 +12,7 @@ const button_1 = document.getElementById("but1"),
       button_4 = document.getElementById("but4");
 
 
+//   an array of objects representing each question and their answers
 const questions = [
     {
         question: 'Commonly used data types Do Not include:',
@@ -64,7 +66,7 @@ let questionIndex = 0;
 let time = 30;
 let score = 0;
 
-
+// function that will begin the clock
 function beginTimer(){
     let timer = setInterval(()=>{
         time -= 1;
@@ -76,11 +78,11 @@ function beginTimer(){
             document.getElementById("fin").style.display = "block";
             document.getElementById("q-box").style.display = "none";
             document.getElementById("intro").style.display = "none";
-
         }
     }, 1000)
 }
 
+// function that will hide the begin game div and display the first question
 function startGame(){
     document.getElementById("intro").style.display = "none";
     document.getElementById("q-box").style.display = "block";
@@ -92,10 +94,9 @@ function startGame(){
     button_4.innerText = questions[questionIndex].answers[3].text;
 
     beginTimer();
-
 }
 
-
+// function that will check if the answer is correct and add to the score
 function checkAnswers (event){
     const user_answer = event.target.innerText;
 
@@ -104,12 +105,10 @@ function checkAnswers (event){
         score += 1;
     } else {
         event.target.style.backgroundColor = "red";
-
-
     }
-
 }
 
+// function that will set the next question when button is clicked
 function nextQuestion (event){
     questionIndex += 1;
 
@@ -120,12 +119,9 @@ function nextQuestion (event){
 
     document.getElementById("final-score").innerText = score;
 
-
-
     if(questionIndex == 5){
         document.getElementById("q-box").style.display = "none";
         document.getElementById("fin").style.display = "block";
-
     } else {
         question_DOM.innerText = questions[questionIndex].question;
         button_1.innerText = questions[questionIndex].answers[0].text;
@@ -135,9 +131,7 @@ function nextQuestion (event){
     }
 }
 
-
-
-
+// function that will display the end screen and send data to the dashboard and local storage
 function endScreen(event){
 
     const initals = document.getElementById("intal").value;
@@ -147,23 +141,18 @@ function endScreen(event){
     } else {
         document.getElementById("fin").style.display = "none";
         document.getElementById("score-co").style.display = "block";
-
         let data = {
             name: initals,
             score: score
         }
-
         localStorage.setItem("scoreboard", JSON.stringify(data));
-
-
         let record = document.createElement("h2");
         record.innerText = `${data.name} - ${data.score}`;
         document.getElementById("score-container").appendChild(record);
-
     }
-    
 }
 
+// function that will start the game all over again
 function playAgain(){
     document.getElementById("score-co").style.display = "none";
     document.getElementById("fin").style.display = "none";
@@ -175,11 +164,9 @@ function playAgain(){
     score = 0;
     questionIndex = 0;
     time = 30;
-
-
 }
 
-
+// clears the score
 function clearScore(){
     document.getElementById("score-container").innerHTML = "";
 }
@@ -193,7 +180,7 @@ document.getElementById("score").addEventListener("click", ()=>{
 })
 
 
-
+// all buttons will envoke the functions above
 start_button.addEventListener('click', startGame);
 button_1.addEventListener("click", checkAnswers);
 button_2.addEventListener("click", checkAnswers);
@@ -203,4 +190,3 @@ next_button.addEventListener("click", nextQuestion);
 submit_button.addEventListener("click", endScreen);
 play_button.addEventListener("click", playAgain);
 clear_button.addEventListener("click", clearScore);
-
